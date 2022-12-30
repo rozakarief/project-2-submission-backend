c9b99e49038de7c69803369e907c512b15c873a2
rozakarief <rozakarief05@gmail.com>
2022-12-30 14:58:24 +0700

    data validation implemented

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,7 +1,4 @@
 /* eslint-disable quotes */
-
-const ClientError = require("../../exceptions/ClientError");
-
 /* eslint-disable no-underscore-dangle */
 class AlbumsHandler {
   constructor(service, validator) {
@@ -10,40 +7,20 @@ class AlbumsHandler {
   }
 
   postAlbumHandler(request, h) {
-    try {
-      this._validator.validateAlbumPayload(request.payload);
-      const { name, year } = request.payload;
+    this._validator.validateAlbumPayload(request.payload);
+    const { name, year } = request.payload;
 
-      const albumId = this._service.addAlbum({ name, year });
+    const albumId = this._service.addAlbum({ name, year });
 
-      const response = h.response({
-        status: "success",
-        message: "Album berhasil ditambahkan",
-        data: {
-          albumId,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: "fail",
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    const response = h.response({
+      status: "success",
+      message: "Album berhasil ditambahkan",
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
   }
 
   getAlbumsHandler() {
@@ -56,95 +33,38 @@ class AlbumsHandler {
     };
   }
 
-  getAlbumByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
-      const album = this._service.getAlbumById(id);
-      return {
-        status: "success",
-        data: {
-          album,
-        },
-      };
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: "fail",
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-      // Server ERROR!
-      const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+  getAlbumByIdHandler(request) {
+    const { id } = request.params;
+    const album = this._service.getAlbumById(id);
+    return {
+      status: "success",
+      data: {
+        album,
+      },
+    };
   }
 
-  putAlbumByIdHandler(request, h) {
-    try {
-      this._validator.validateAlbumPayload(request.payload);
-      const { id } = request.params;
+  putAlbumByIdHandler(request) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
 
-      this._service.editAlbumById(id, request.payload);
+    this._service.editAlbumById(id, request.payload);
 
-      return {
-        status: "success",
-        message: "Album berhasil diperbarui",
-      };
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: "fail",
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-      // Server ERROR!
-      const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    return {
+      status: "success",
+      message: "Album berhasil diperbarui",
+    };
   }
 
-  deleteAlbumByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
+  deleteAlbumByIdHandler(request) {
+    const { id } = request.params;
 
-      this._service.deleteAlbumById(id);
+    this._service.deleteAlbumById(id);
 
-      return {
-        status: "success",
-        message: "Album berhasil dihapus",
-      };
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: "fail",
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-      // Server ERROR!
-      const response = h.response({
-        status: "error",
-        message: "Maaf, terjadi kegagalan pada server kami.",
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    return {
+      status: "success",
+      message: "Album berhasil dihapus",
+    };
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const Hapi = require("@hapi/hapi");
 
 // albums
 const albums = require("./api/albums");
+const ClientError = require("./exceptions/ClientError");
 const AlbumsService = require("./services/inMemory/AlbumsService");
 const AlbumsValidator = require("./validator/albums");
 
@@ -25,6 +26,35 @@ const init = async () => {
       service: albumsService,
       validator: AlbumsValidator,
     },
+  });
+
+  server.ext("onPreResponse", (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: "fail",
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: "error",
+        message: "terjadi kegagalan pada server kami",
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
   });
 
   await server.start();
