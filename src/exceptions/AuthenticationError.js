ad088b9cdb0f698b5e86c855ae301f7ad3ce85e4
rozakarief <rozakarief05@gmail.com>
2023-01-02 15:02:09 +0700

    authentication implemented

---
+++ src/exceptions/AuthenticationError.js
@@ -0,0 +1,10 @@
+/* eslint-disable quotes */
+const ClientError = require("./ClientError");
+
+class AuthenticationError extends ClientError {
+  constructor(message) {
+    super(message, 401);
+    this.name = "AuthenticationError";
+  }
+}
+module.exports = AuthenticationError;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 const ClientError = require("./exceptions/ClientError");
 
 // albums
@@ -21,10 +22,17 @@ const users = require("./api/users");
 const UsersService = require("./services/postgres/UsersService");
 const UsersValidator = require("./validator/users");
 
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -34,6 +42,30 @@ const init = async () => {
         origin: ["*"],
       },
     },
+  });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy("openmusicsapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -57,6 +89,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
